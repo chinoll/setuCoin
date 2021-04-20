5dc39de549cc33738683816a76b301fa55114001
chinoll <chinoll@protonmail.com>
2021-04-20 23:10:45 +0800

    jit

--- block.py
+++ block.py
@@ -1,6 +1,7 @@
 import hashlib
 import time
 import json
+from numba import jit
 class Block:
     def __init__(self,index,timestamp,sha256,previous_hash,data):
         self.index = index
@@ -10,8 +11,9 @@ class Block:
         self.timestamp = timestamp
 
 current_milli_time = lambda: int(round(time.time() * 1000))
-def calcSHA256(index,timestamp,previous_hash,data):
-    print(timestamp)
+
+@jit()
+def calcSHA256(index:int,timestamp:int,previous_hash:str,data:str) -> str:
     sha = hashlib.sha256()
     sha.update((str(index) + str(timestamp) + previous_hash + data).encode('utf-8'))
     return sha.hexdigest()
